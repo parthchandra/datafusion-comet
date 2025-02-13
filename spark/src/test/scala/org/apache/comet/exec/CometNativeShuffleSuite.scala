dc6ac2776441cca481cdc0babf52b74ff302982c
Parth Chandra <parthc@apache.org>
2025-02-13 10:30:57 +0530

    sync with changes in uint_type_tests

--- spark/src/test/scala/org/apache/comet/exec/CometNativeShuffleSuite.scala
+++ spark/src/test/scala/org/apache/comet/exec/CometNativeShuffleSuite.scala
@@ -71,8 +71,7 @@ class CometNativeShuffleSuite extends CometTestBase with AdaptiveSparkPlanHelper
           allTypes.map(i => s"_$i").foreach { c =>
             withSQLConf(
               CometConf.COMET_EXEC_ENABLED.key -> execEnabled.toString,
-              "parquet.enable.dictionary" -> dictionaryEnabled.toString,
-              CometConf.COMET_SCAN_ALLOW_INCOMPATIBLE.key -> "true") {
+              "parquet.enable.dictionary" -> dictionaryEnabled.toString) {
               readParquetFile(path.toString) { df =>
                 val shuffled = df
                   .select($"_1")
